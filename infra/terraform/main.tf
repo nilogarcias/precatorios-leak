e8d8d94558d05c1f7c80c518c857c19a95f79cfe
Nilo Garcia <nilo.garcia@quintoandar.com.br>
2025-05-18 16:51:48 -0300

    Fixing github infra workflow to use Service Principal

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,4 +2,3 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
-
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -7,3 +7,23 @@ variable "prefix" {
   description = "Prefix for the resource names"
   default     = "precatorios"
 }
+
+variable "az_subscription_id" {
+  description = "Azure Subscription ID"
+  sensitive   = true
+}
+
+variable "az_client_id" {
+  description = "Azure Service Principal App ID"
+  sensitive   = true
+}
+
+variable "az_client_secret" {
+  description = "Azure Service Principal Password"
+  sensitive   = true
+}
+
+variable "az_tenant_id" {
+  description = "Azure Tenant ID"
+  sensitive   = true
+}
