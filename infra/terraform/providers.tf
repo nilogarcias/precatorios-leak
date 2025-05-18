d5ca88825bf9c2843e0a6ee5ab230b6276895fdc
Nilo Garcia <nilo.garcia@quintoandar.com.br>
2025-05-18 17:06:03 -0300

    Changing the azurerm to get the credentials from Service Principal

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 4.1.0"
     }
   }
   required_version = ">= 1.1.0"
@@ -10,5 +10,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
 
+  # Add this variables for non iterative authentication
+  subscription_id = var.az_subscription_id
+  client_id       = var.az_client_id
+  client_secret   = var.az_client_secret
+  tenant_id       = var.az_tenant_id
+}
