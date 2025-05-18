8a6b20ea08cfbf2af1d0705092bbc516e264ece0
Nilo Garcia <nilo.garcia@quintoandar.com.br>
2025-05-18 16:04:09 -0300

    Initial infra project commit

---
+++ infra/azure-cloud/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure region"
+  default     = "brazilsouth"
+}
+
+variable "prefix" {
+  description = "Prefix for the resource names"
+  default     = "precatorios"
+}
